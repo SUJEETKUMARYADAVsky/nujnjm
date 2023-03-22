739fd0ac0c78f0ef3253af3a8e95337f445fd9f2
Sujeet Kumar Yadav <sujeetiit54321@gmail.com>
2023-03-22 15:23:37 +0530

    Initial commit

---
+++ area_of_triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ int main (){
+    float a,b,h;
+    cout<<"enter base and height";
+    cin>>b>>h;
+    a=(b*h)/2;
+    cout<<"area is"<<a;
+    return 0;
+
+ }
