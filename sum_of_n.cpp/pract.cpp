9d0c4359dbb18f3e8f47596eb37dd1d5b6664f28
Sujeet Kumar Yadav <sujeetiit54321@gmail.com>
2023-07-06 16:09:37 +0530

    new changes

---
+++ sum_of_n.cpp/pract.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int x,y;
+    cout <<"Enter two number";
+    cin >> x >> y;
+    if (x>y)
+
+    {
+        cout << "mai is "<< x<< endl;
+
+    }else{
+        cout <<"max is "<< y<< endl;
+    }
+    return 0;
+}
---
+++ sum_of_n.cpp/sumoftwonum.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num1,num2,sum;
+    cout << "Enter num1"<< endl;
+    cin >> num1;
+    cout <<"Enter num2"<< endl;
+    cin >> num2;
+    sum =num1+num2;
+    cout<< "sum "<< num1 <<"and"<<num2<< "is"<<sum<< endl;
+    return 0;
+}ö
