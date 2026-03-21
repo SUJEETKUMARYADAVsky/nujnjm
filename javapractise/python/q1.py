bab6e4f4635e8bc4a2f2c1992bf49b12cb37ee04
SUJEETKUMARYADAVsky <112688382+SUJEETKUMARYADAVsky@users.noreply.github.com>
2026-03-21 22:27:14 +0530

    pract

---
+++ javapractise/python/q1.py
@@ -0,0 +1,6 @@
+'''The provided code stub reads two integers, and , from STDIN. Add logic to print two lines. The first line should contain the result of integer division, // . The second line should contain the result of float division, / . No rounding or formatting is necessary. Example The result of the integer division . The result of the float division is .'''
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a / b)
